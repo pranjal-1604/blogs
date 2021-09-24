31f463e0ab8cb9e0155ab4305fef0fdbc155459e
Pranjal Singh <pranjal.dalu@gmail.com>
2021-09-24 11:14:54 +0530

    lodash added

--- app.js
+++ app.js
@@ -3,6 +3,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+var _ = require("lodash");
 
 const homeStartingContent =
   "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
@@ -32,6 +33,17 @@ app.get("/contact", (req, res) => {
 app.get("/compose", (req, res) => {
   res.render("compose");
 });
+
+app.get("/posts/:postName", (req, res) => {
+  const requestedTitle = _.lowerCase(req.params.postName);
+  posts.forEach((post) => {
+    const storedTitle = _.lowerCase(post.title);
+    if (storedTitle === requestedTitle) {
+      res.render("post", { title: post.title, content: post.content });
+    }
+  });
+});
+
 app.post("/compose", (req, res) => {
   const post = {
     title: req.body.postTitle,
@@ -39,14 +51,6 @@ app.post("/compose", (req, res) => {
   };
   posts.push(post);
   res.redirect("/");
-});
-
-app.get("/posts/:postName", (req, res) => {
-  posts.forEach((post) => {
-    if (post.title === req.params.postName) {
-      res.render("post", { post: post });
-    }
-  });
 });
 
 app.listen(3000, function () {
